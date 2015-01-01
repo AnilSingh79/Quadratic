48c57d133087f47dadfa476fcd62e66cbefd1aa5
AnilSingh79 <singh.ap79@gmail.com>
2015-01-01 10:27:05 -0600

    Hello World : First Commit

---
+++ AbcHelloWorld.java
@@ -0,0 +1,16 @@
+/**
+Author : Anil Singh, Ershtex Club
+File   : AbcHelloWorld.java
+Desc   : A simple HelloWorld program
+Usage  : java AbcHelloWorld
+***************************************************/
+import java.io.*;
+
+public class AbcHelloWorld{
+    /*The main function*/
+    static public void main(String args){
+	/*Do it*/
+	System.out.println("Hello World!");
+    }//main function ends here.
+}//class AbcHelloWorld ends here.
+
